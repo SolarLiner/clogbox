9291fedcf71c3f45e203dee6ddc8f9c743d82937
Nathan Graule <solarliner@gmail.com>
2025-04-22 23:52:04 +0200

    wip(math): newton-rhapson, python sympy codegen, and an example

--- crates/clogbox-math/src/root_eq/mod.rs
+++ crates/clogbox-math/src/root_eq/mod.rs
@@ -5,14 +5,6 @@ pub trait Differentiable {
     /// Scalar type of the equation
     type Scalar: Clone;
 
-    /// Evaluates the function at a point
-    fn eval(&self, x: Self::Scalar) -> Self::Scalar;
-
-    /// Evaluates the derivative of the function at a point
-    fn derivative(&self, x: Self::Scalar) -> Self::Scalar;
-
     /// Evaluates both the function and its derivative at a point
-    fn eval_with_derivative(&self, x: Self::Scalar) -> (Self::Scalar, Self::Scalar) {
-        (self.eval(x.clone()), self.derivative(x))
-    }
+    fn eval_with_derivative(&self, x: Self::Scalar) -> (Self::Scalar, Self::Scalar);
 }
---
+++ examples/nr-clipper/src/lib.rs
@@ -0,0 +1,30 @@
+use clogbox_clap::main_thread::{Plugin, PortLayout};
+use clogbox_clap::processor::{HostSharedHandle, PluginError};
+use clogbox_clap::{export_plugin, PluginMeta};
+use clogbox_module::Module;
+
+mod dsp;
+mod gen;
+
+pub struct NrClipper;
+
+impl PluginMeta for NrClipper {
+    const ID: &'static str = "dev.solarliner.clogbox.nr-clipper";
+    const NAME: &'static str = "NR Clipper";
+}
+
+impl Plugin for NrClipper {
+    type Dsp = dsp::Dsp;
+    type Params = dsp::Params;
+
+    const INPUT_LAYOUT: &'static [PortLayout<<Self::Dsp as Module>::AudioIn>] =
+        &[PortLayout::STEREO.main().named("Input")];
+    const OUTPUT_LAYOUT: &'static [PortLayout<<Self::Dsp as Module>::AudioOut>] =
+        &[PortLayout::STEREO.main().named("Output")];
+
+    fn create(_: HostSharedHandle) -> Result<Self, PluginError> {
+        Ok(Self)
+    }
+}
+
+export_plugin!(NrClipper);

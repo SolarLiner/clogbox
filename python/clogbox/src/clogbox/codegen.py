ead5b229c6d19d4f98b1a7015c823e392eb6cd7e
Nathan Graule <solarliner@gmail.com>
2025-05-02 11:28:39 +0200

    feat(codegen): improve codegen module

--- python/clogbox/src/clogbox/codegen.py
+++ python/clogbox/src/clogbox/codegen.py
@@ -1,9 +1,17 @@
-import functools
-import operator
-from typing import Optional, Tuple, Iterable, List, Union
+import io
+import typing
+from dataclasses import dataclass
+from typing import Optional, Tuple, Union
 
 import sympy as sp
+from sympy.codegen.ast import value_const
+from sympy.codegen import ast
+from sympy.printing.codeprinter import requires
 from sympy.printing.rust import RustCodePrinter
+import sympy.matrices.expressions as mat
+from sympy.utilities import codegen
+
+from clogbox.nodes import Tuple
 
 RUST_CODE_PREAMBLE = """
 //! Generated source code with the `clogbox` Python package, to use with the `clogbox` project.
@@ -22,6 +30,20 @@ use nalgebra as na;
 
 
 class ClogboxRustCodePrinter(RustCodePrinter):
+    tab = " " * 4
+
+    def __init__(self, settings=None):
+        if settings is None:
+            settings = {}
+        user_functions = settings.pop("user_functions", {})
+        user_functions.update({"Determinant": "determinant"})
+        settings["user_functions"] = user_functions
+        super().__init__(settings)
+
+        self.uses: set[str] = set()
+        self.type_params: set[str] = set()
+
+    @requires(type_params={"Float"})
     def _print_Pow(self, expr):
         base_code = self._print(expr.base)
         exp = expr.exp
@@ -30,9 +52,11 @@ class ClogboxRustCodePrinter(RustCodePrinter):
         else:
             return super()._print_Pow(expr)
 
+    @requires(type_params={"CastFrom<f64>"}, uses={"az::CastFrom"})
     def _print_Integer(self, expr, _type=False):
         return f"T::cast_from({float(expr)})"
 
+    @requires(type_params={"CastFrom<f64>"}, uses={"az::CastFrom"})
     def _print_Float(self, expr, **kwargs):
         return f"T::cast_from({float(expr)})"
 
@@ -40,6 +64,7 @@ class ClogboxRustCodePrinter(RustCodePrinter):
         p, q = tuple(self._print(sp.Integer(x)) for x in (expr.p, expr.q))
         return f"{p}/{q}"
 
+    @requires(type_params={"na::Scalar"}, uses={"nalgebra as na"})
     def _print_MatrixBase(self, mat: sp.MatrixBase):
         values = ", ".join(self._print(x) for x in mat)
         cdim = f"na::Const<{mat.cols}>"
@@ -50,20 +75,121 @@ class ClogboxRustCodePrinter(RustCodePrinter):
             mtype = f"na::OMatrix::<T, {rdim}, {cdim}>"
         return f"{mtype}::new({values})"
 
+    @requires(type_params={"FloatConst"}, uses={"num_traits::FloatConst"})
     def _print_NaN(self, expr, _type=False):
         return "T::nan()"
 
+    @requires(type_params={"FloatConst"}, uses={"num_traits::FloatConst"})
     def _print_Exp1(self, expr, _type=False):
         return "T::E()"
 
+    @requires(type_params={"FloatConst"}, uses={"num_traits::FloatConst"})
     def _print_Pi(self, expr, _type=False):
         return "T::PI()"
 
+    @requires(type_params={"FloatConst"}, uses={"num_traits::FloatConst"})
     def _print_Infinity(self, expr, _type=False):
         return "T::INFINITY()"
 
+    @requires(type_params={"FloatConst"}, uses={"num_traits::FloatConst"})
     def _print_NegativeInfinity(self, expr, _type=False):
         return "T::NEG_INFINITY()"
+
+    def _print_Tuple(self, expr: Tuple):
+        return "({values})".format(values=", ".join(self._print(x) for x in expr.args))
+
+    def _print_FunctionDefinition(self, fdecl: ast.FunctionDefinition):
+        self.type_params.clear()
+
+        params = ", ".join(self._print_Variable(var, _type=True) for var in fdecl.parameters)
+        rtype = self._print_Type(fdecl.return_type)
+        body = self._indent_codestring(self._print(fdecl.body))
+        typeparams = "<T{bounds}>".format(bounds=": " + " + ".join(self.type_params) if len(self.type_params) > 0 else "")
+        return f"""fn {fdecl.name}{typeparams}({params}) -> {rtype} {{\n{body}\n}}"""
+
+    def _print_Type(self, t: ast.Type):
+        if isinstance(t, ast.ComplexBaseType):
+            self.uses.add("nalgebra as na")
+            return "na::Complex<T>"
+        if isinstance(t, ast.SignedIntType):
+            return "i64"
+        if isinstance(t, ast.UnsignedIntType):
+            return "u64"
+        return "T"
+
+    def _print_Declaration(self, decl: ast.Declaration, _type=False):
+        if value_const in decl.variable.attrs:
+            mut = ""
+        else:
+            mut = " mut"
+        return self._get_statement(f"let{mut} {self._print_Variable(decl.variable, _type=_type)} = {self._print(decl.variable.value)}")
+
+    def _print_Variable(self, x: ast.Variable, _type=False):
+        if _type:
+            return f"{x.symbol.name}: {self._print_Type(x.type)}"
+        return x.symbol.name
+
+    def _print_While(self, node: ast.While):
+        condition = self._print(node.condition)
+        body = self._indent_codestring(self._print(node.body))
+        return f"while {condition} {{\n{body}\n}}"
+
+    def _print_Return(self, node: ast.Return, is_last_statement=False):
+        if is_last_statement:
+            return self._print(getattr(node, "return"))
+        return self._get_statement(f"return {self._print(getattr(node, 'return'))}")
+
+    def _print_Determinant(self, expr: mat.Determinant):
+        return self._print_Function(expr)
+
+    def _indent_codestring(self, codestring):
+        return '\n'.join([self.tab + line for line in codestring.split('\n')])
+
+
+class ClogboxCodegen(codegen.RustCodeGen):
+    printer: ClogboxRustCodePrinter
+    def __init__(self, **kwargs):
+        kwargs["printer"] = ClogboxRustCodePrinter()
+        super().__init__(**kwargs)
+
+    def get_prototype(self, routine: codegen.Routine):
+        def print_arg(arg: codegen.InputArgument | codegen.InOutArgument):
+            self.printer.type_params.clear()
+
+            typ = get_type(arg.name)
+            if typ.startswith("na::"):
+                self.printer.uses.add("nalgebra as na")
+                self.printer.type_params.add("na::Scalar")
+            return f"{arg.name}: {typ}"
+
+        replacement, results = sp.cse(result.expr for result in typing.cast(list[codegen.Result], routine.results))
+        codeblock = ast.CodeBlock(*(ast.Assignment(lhs, rhs) for lhs, rhs in replacement), ast.Return(*results))
+        _body = self.printer.indent_code(self.printer.doprint(codeblock))
+        args = ", ".join(print_arg(arg) for arg in routine.arguments)
+        typeparams = "<T{bounds}>".format(bounds=": " + " + ".join(self.printer.type_params) if len(
+            self.printer.type_params) > 0 else "")
+        return f"fn {routine.name}{typeparams}({args}) -> T"
+
+
+def render_as_module(content, printer: Optional[ClogboxRustCodePrinter]=None) -> str:
+    if not printer:
+        printer = ClogboxRustCodePrinter()
+    str = printer.doprint(content)
+    imports = "\n".join(f"use {module};" for module in printer.uses)
+    return f"{imports}\n\n{str}"
+
+
+def codegen_module(routines, printer: Optional[ClogboxRustCodePrinter]=None, codegen: Optional[ClogboxCodegen]=None,
+                   project: str = "clogbox") -> str:
+    if not printer:
+        printer = ClogboxRustCodePrinter()
+    if not codegen:
+        codegen = ClogboxCodegen(printer=printer)
+
+    [(_, contents)] = codegen.write(routines, "")  # type: str
+    imports = "\n".join(f"use {module};" for module in codegen.printer.uses)
+    end_of_comment = contents.find("*/") + 3 # 2 chars + newline
+    return contents[:end_of_comment] + "\n" + imports + "\n\n" + contents[end_of_comment:]
 
 
 def get_type(e: sp.Basic):
@@ -73,115 +199,40 @@ def get_type(e: sp.Basic):
     :return: Rust type representation
     """
     match e:
-        case sp.MatrixBase(cols=c, rows=r):
+        case sp.MatrixBase(cols=c, rows=r) | sp.MatrixSymbol(cols=c, rows=r):
             return f"na::OMatrix<T, na::Const<{r}>, na::Const<{c}>>"
         case _:
             return "T"
 
 
-def generate_statements(
-    *exprs: Tuple[str, sp.Expr], printer: Optional[RustCodePrinter] = None
-) -> Iterable[str]:
-    """
-    Generate Rust statements from a list of SymPy expressions with their assignments.
+@dataclass()
+class IndentStream(io.TextIOBase):
+    stream: io.TextIOBase
+    indent: str = " " * 4
 
-    To work properly, the resulting source code must be compiled with the `num-traits` crate and also be within a file
-    that also has the contents of the `RUST_CODE_PREAMBLE` constant present.
+    def write(self, s: str) -> int:
+        return self.stream.write(s.replace("\n", "\n" + self.indent))
 
-    :param exprs: Expressions to be turned into statements, with their assignment bindings.
-    :param printer: Printer instance to use, or None to create a new instance.
-    :return: The generated code, yielding one line at a time (newlines not included).
-    """
-    if not printer:
-        printer = ClogboxRustCodePrinter()
-    extra_bindings, return_values = sp.cse([v for _, v in exprs])
-    for name, expr in extra_bindings:
-        yield f"let {name} = {printer.doprint(expr)};"
-    for name, expr in zip((k for k, _ in exprs), return_values):
-        yield f"let {name} = {printer.doprint(expr)};"
+    def flush(self):
+        self.stream.flush()
+
+    def close(self):
+        self.stream.close()
 
 
-def generate_function(
-    name: str, *exprs: sp.Expr, printer: Optional[RustCodePrinter] = None
-) -> Iterable[str]:
-    """
-    Generate a Rust function from a SymPy expression.
-
-    To work properly, the resulting source code must be compiled with the `num-traits` crate and also be within a file
-    that also has the contents of the `RUST_CODE_PREAMBLE` constant present.
-    :param exprs: List of expressions. Will be returned as a tuple in-order.
-    :param name: Name of the function to generate
-    :param printer: Printer instance to use, or None to create a new instance.
-    :return: The generated code, yielding one line at a time (newlines not included).
-    """
-    if not printer:
-        printer = ClogboxRustCodePrinter()
-    bindings, return_expr = sp.cse(exprs)
-
-    free_args = sorted(
-        functools.reduce(operator.or_, (e.free_symbols for e in exprs), set()),
-        key=lambda s: s.name,
-    )
-
-    args = ", ".join(f"{name}: {get_type(name)}" for name in free_args)
-    if len(exprs) == 1:
-        rtype = get_type(*return_expr)
-        return_expr = printer.doprint(*return_expr)
-    else:
-        rtype = ", ".join(get_type(e) for e in exprs)
-        rtype = f"({rtype})"
-        return_expr = ", ".join(printer.doprint(e) for e in return_expr)
-        return_expr = f"({return_expr})"
-
-    if any(isinstance(x, sp.MatrixBase) for x in exprs):
-        na_traits = "na::Scalar + "
-    else:
-        na_traits = ""
-
-    yield f"pub fn {name}<T: {na_traits}Float + FloatConst + CastFrom<f64>>({args}) -> {rtype} {{"
-    for line in generate_statements(
-        *((name, expr) for name, expr in bindings), printer=printer
-    ):
-        yield "    " + line
-
-    yield f"    {return_expr}"
-    yield "}"
-
-
-def generate_functions(
-    *exprs: Tuple[str, sp.Expr], printer: Optional[RustCodePrinter] = None
-) -> Iterable[str]:
-    for name, expr in exprs:
-        yield from generate_function(name, expr, printer=printer)
-        yield ""
-
-
-def generate_module(
-    exprs: List[Tuple[str, sp.Expr]], printer: Optional[RustCodePrinter] = None
-) -> Iterable[str]:
-    """
-    Generates Rust module code for a list of symbolic expressions.
-
-    :param exprs: List of tuples where each tuple contains a function name (str) and
-        its corresponding symbolic expression computed with SymPy.
-    :param printer: Optional instance of `RustCodePrinter` used to print symbolic
-        expressions as Rust-compatible code. If not provided, a default printer will
-        be used.
-    :return: Iterable of strings, each representing a line of the generated Rust
-        module code.
-    """
-    yield from RUST_CODE_PREAMBLE.splitlines()
-    yield ""
-    yield from generate_functions(*exprs, printer=printer)
-
+def generate_codeblock(exprs: list[sp.Expr]) -> ast.CodeBlock:
+    replacements, exprs = sp.cse(exprs)
+    return ast.CodeBlock(*(ast.Assignment(lhs, rhs) for lhs, rhs in replacements), Tuple(exprs))
 
 def generate_differentiable(
+    f: io.TextIOBase,
     expr: Union[sp.Expr, sp.Eq],
     variable: Union[sp.Symbol, sp.MatrixBase],
     struct_name="Equation",
     printer: Optional[RustCodePrinter] = None,
+    codegen: Optional[ClogboxCodegen] = None,
     runtime_invert=False,
-) -> Iterable[str]:
+):
     """
     Generate Rust code for a specific equation that is differentiable once about a variable. This function will
     implement the `clogbox_math::root_eq::Differentiable` trait for the generated struct.
@@ -189,16 +240,19 @@ def generate_differentiable(
     The resulting source code needs the `clogbox_math` and `num-traits` crates. The final module must also have the
     contents of the `DIFFERENTIABLE_CODE_PREAMBLE` constant present.
 
+    :param f: Stream to write the code to.
     :param expr: Expression to implement the trait for.
     :param variable: Variable about which the equation is differentiable.
     :param struct_name: Name of the struct to generate (defaults to "Equation").
     :param printer: Optional instance of `RustCodePrinter` used to print symbolic
         expressions as Rust-compatible code. If not provided, a default printer will
         be used.
-    :return: Generated lines of code
     """
     if not printer:
         printer = ClogboxRustCodePrinter()
+    if not codegen:
+        codegen = ClogboxCodegen(printer=printer)
+
     if isinstance(expr, sp.Eq):
         expr = expr.rhs - expr.lhs
 
@@ -208,46 +262,58 @@ def generate_differentiable(
         var_symbols = {variable}
     fields = sorted(expr.free_symbols - var_symbols, key=lambda s: s.name)
 
-    yield f"pub struct {struct_name}<T> {{"
+    f.write(f"pub struct {struct_name}<T> {{")
     for field in fields:
-        yield f"    pub {str(field)}: {get_type(field)},"
-    yield "}"
-    yield ""
+        f.write(f"    pub {str(field)}: {get_type(field)},")
+    f.write( "}")
+    f.writelines([""])
+
     if isinstance(expr, sp.MatrixBase):
         vtype = "na::OVector<Self::Scalar, Self::Dim>"
         mtype = "na::OMatrix<Self::Scalar, Self::Dim, Self::Dim>"
         vview_type = "na::VectorView<Self::Scalar, Self::Dim>"
-        yield f"impl<T: Copy + na::Scalar + na::RealField + FloatConst + CastFrom<f64>> root_eq::MultiDifferentiable for {struct_name}<T> {{"
-        yield f"    type Scalar = T;"
-        yield f"    type Dim = na::Const<{expr.rows}>;"
-        yield ""
-        yield f"    fn eval_with_inv_jacobian(&self, matrix: {vview_type}) -> ({vtype}, {mtype}) {{"
+        f.write(
+f"""
+impl<T: Copy + na::Scalar + na::RealField + FloatConst + CastFrom<f64>> root_eq::MultiDifferentiable for {struct_name}<T> {{
+    type Scalar = T;
+    type Dim = na::Const<{expr.rows}>;
+    
+    fn eval_with_inv_jacobian(&self, matrix: {vview_type}) -> ({vtype}, {mtype}) {{"""
+        )
         for i, name in enumerate(variable):
-            yield f"        let {str(name)} = matrix[{i}];"
+            f.write(f"        let {str(name)} = matrix[{i}];")
         for field in fields:
-            yield f"        let {str(field)} = self.{str(field)};"
+            f.write(f"        let {str(field)} = self.{str(field)};")
 
+        s = IndentStream(f, indent=" " * 8)
         if runtime_invert:
             j = expr.jacobian(variable)
-            for line in generate_statements(("f", expr), ("df", j), printer=printer):
-                yield "        " + line
-            yield "        let df = df.try_inverse().unwrap();"
+            block = generate_codeblock([expr, j])
+            s.write(printer.doprint(block, assign_to="(f, df)"))
+            s.write("        (f, df.try_inverse().unwrap())")
         else:
             inv_j = expr.jacobian(variable).inverse()
-            for line in generate_statements(("f", expr), ("df", inv_j), printer=printer):
-                yield "        " + line
+            block = generate_codeblock([expr, inv_j])
+            s.write(printer.doprint(block))
 
     else:
-        yield f"impl<T: Float + FloatConst + CastFrom<f64>> root_eq::Differentiable for {struct_name}<T> {{"
-        yield f"    type Scalar = T;"
-        yield ""
-        yield f"    fn eval_with_derivative(&self, {str(variable)}: T) -> (T, T) {{"
+        f.write(
+f"""
+impl<T: Copy + na::Scalar + na::RealField + FloatConst + CastFrom<f64>> root_eq::Differentiable for {struct_name}<T> {{
+    type Scalar = T;
+
+    fn eval_with_derivative(&self, {str(variable)}: T) -> (T, T) {{
+"""
+        )
         for field in fields:
-            yield f"        let {str(field)} = self.{str(field)};"
+            f.write(f"        let {str(field)} = self.{str(field)};")
 
         diff = sp.diff(expr, variable)
-        for line in generate_statements(("f", expr), ("df", diff), printer=printer):
-            yield "        " + line
-    yield "        (f, df)"
-    yield "    }"
-    yield "}"
+        block = generate_codeblock([expr, diff])
+        f.write(printer.doprint(block))
+        f.write(
+"""
+    }
+}
+"""
+        )
--- python/clogbox/tests/test_sympy_codegen.py
+++ python/clogbox/tests/test_sympy_codegen.py
@@ -1,14 +1,64 @@
+from io import StringIO
+
 import sympy as sp
+from sympy.codegen import ast
+from sympy.utilities.codegen import RustCodeGen, Routine
 
-from clogbox.codegen import ClogboxRustCodePrinter
+from clogbox.codegen import ClogboxRustCodePrinter, ClogboxCodegen, render_as_module, codegen_module, \
+    generate_differentiable
+
+import pytest
 
 
-def test_asinh_derivative():
-    printer = ClogboxRustCodePrinter()
+@pytest.fixture
+def printer() -> ClogboxRustCodePrinter:
+    return ClogboxRustCodePrinter()
+
+
+@pytest.fixture
+def codegen(printer: ClogboxRustCodePrinter) -> ClogboxCodegen:
+    return ClogboxCodegen(printer=printer)
+
+
+def test_asinh_derivative(printer: ClogboxRustCodePrinter, snapshot: str):
     u = sp.Symbol('u', real=True)
     expr = 1 / sp.sqrt(u ** 2 + 1)
     actual = printer.doprint(expr)
-    expected = "(u.powi(2) + T::cast_from(1.0)).sqrt().recip()"
-    assert expected == actual
+    # expected = "(u.powi(2) + T::cast_from(1.0)).sqrt().recip()"
+    assert snapshot == actual
 
 
+def test_generate_differentiable(codegen: ClogboxCodegen, snapshot: str):
+    u = sp.Symbol('u', real=True)
+    expr = sp.asinh(u) - sp.tanh(u)
+
+    actual = StringIO()
+    generate_differentiable(actual, expr, u, printer=codegen.printer, codegen=codegen)
+    assert snapshot == actual.getvalue()
+
+
+def test_newton_rhapson_function(printer: ClogboxRustCodePrinter, snapshot: str):
+    from sympy.codegen.algorithms import newtons_method_function
+
+    u = sp.Symbol('u', real=True)
+    expr = sp.asinh(u) - sp.tanh(u)
+    code = newtons_method_function(expr, u, cse=True)
+    actual = printer.doprint(code)
+    assert snapshot == actual
+
+
+def test_matrix_expression(printer: ClogboxRustCodePrinter, snapshot: str):
+    u = sp.Symbol("u")
+    X = sp.Matrix([[u, u ** 2], [u ** 3, u ** 4]])
+    root_eq = sp.Determinant(X) - sp.tanh(u)
+    actual = printer.doprint(root_eq)
+    assert snapshot == actual
+
+
+def test_matrix_routine(codegen: ClogboxCodegen, snapshot: str):
+    X = sp.MatrixSymbol('X', 2, 2)
+    y = sum(X) / sp.Determinant(X)
+
+    routine = codegen.routine("matrix_routine", y, [X], [])
+    actual = codegen_module([routine], codegen=codegen)
+    assert snapshot == actual
